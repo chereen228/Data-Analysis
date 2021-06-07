54acd893832e302db10b41b8f3904b094e7f7705
Chaerin Shin <chereen228@naver.com>
2021-06-08 08:49:55 +0900

    크롤링

--- 05.Crawling/03_Google.py
+++ 05.Crawling/03_Google.py
@@ -4,21 +4,22 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get('http://www.google.com')
-iver = webdriver.Chrome('./chromedriver')
-driver.get('http://www.google.com')
-time.sleep(0.5)         # 0.5초 기다림
+time.sleep(0.5)     # 0.5 초 기다림
 
-search_box = driver.find_element_by_css_selector('input.gLFyf.gsfi')
+search_box = driver.find_element_by_css_selector('.gLFyf.gsfi')
 search_box.send_keys('ChromeDriver')
-search_box.send_keys(Keys.ENTER)    #Keys.RETURN
+search_box.send_keys(Keys.ENTER)    # Keys.RETURN
 time.sleep(1)
 
-div = driver.find_elements_by_css_selector("div#rso>div.g")
-len(div)
-
+divs = driver.find_elements_by_css_selector('div#rso>div.g')
 for div in divs:
-    title = div.find_element_by_css_selector('.LC20lb.DKV0Md').text
-    content = div.find_element_by_css_selector('.VwiC3b.yXK7lf.MUxGbd.yDYNvb.lyLwlc').text
-    print(title)
-    print(content)
+    try:
+        title = div.find_element_by_css_selector('.LC20lb.DKV0Md').text
+        content = div.find_element_by_css_selector('.VwiC3b.yXK7lf.MUxGbd.yDYNvb.lyLwlc').text
+        print(title)
+        print(content)
+        print('==============================')
+    except:
+        pass
 
+driver.close()
